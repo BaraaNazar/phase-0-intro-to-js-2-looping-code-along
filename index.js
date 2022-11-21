b24af4b700cab68ac6352c208a74f5a3bc0e9f6e
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-11-21 18:59:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let arrayMess = [];
+function writeCards(array, event){
+for (let i=0; i < array.length; i++){
+    arrayMess[i]=`Thank you, ${array[i]}, for the wonderful ${event} gift!`
+}
+return arrayMess
+}
+
+
+function countDown(num){
+    let result = [num]
+    for(let i = 0 ; i <= result; i++){
+    console.log(num--);
+    } 
+}
+countDown(10)
